72f783a16a5fe9e697de542a9636ea9147baea29
Joingl <joel.inglin@uzh.ch>
2023-08-28 14:42:58 +0200

    Cleaning up CLIRunner code.

--- src/main/java/com/example/automator/helper/CLIRunner.java
+++ src/main/java/com/example/automator/helper/CLIRunner.java
@@ -8,50 +8,30 @@ import org.apache.commons.lang3.SystemUtils;
 
 public class CLIRunner {
     public void runCommand(String cmd) {
-        if (SystemUtils.IS_OS_WINDOWS) {
-            try {
-                System.out.println("Building process for Windows...");
-                ProcessBuilder pb = new ProcessBuilder();
-                //System.out.println(System.getenv()); //Prints SYSTEMPATH, enables manual check if NetLogo is part of it.
+        try {
+            System.out.println("Building process for Windows...");
+            ProcessBuilder pb = new ProcessBuilder();
+            //System.out.println(System.getenv()); //Prints SYSTEMPATH, enables manual check if NetLogo is part of it.
+            if (SystemUtils.IS_OS_WINDOWS) {
                 pb.command("cmd.exe", "/c", "behaviorsearch_headless.bat " + cmd);
-                
-                Process process = pb.start();
-                //StringBuilder output = new StringBuilder();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
-                //System.out.println(output);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        else if (SystemUtils.IS_OS_UNIX) {
-            try {
-                System.out.println("Building process for UNIX-System...");
-                ProcessBuilder pb = new ProcessBuilder();
-                //System.out.println(System.getenv()); //Prints SYSTEMPATH, enables manual check if NetLogo is part of it.
+            } else if (SystemUtils.IS_OS_LINUX) {
                 pb.command("/bin/bash", "-c", "behaviorsearch_headless.sh " + cmd);
-                //pb.command("sh", "-c", "behaviorsearch_headless.sh " + cmd); //TRY THIS LINE ALTERNATIVELY
-                
-                Process process = pb.start();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else {
+                throw new IOException("OS not compatible.");
             }
-        } else {
-            System.out.println("OS not compatible.");
-        }
 
+            Process process = pb.start();
+            //StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            //System.out.println(output);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

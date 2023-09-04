77eac1bdd1cbec1535e4de724a2ad2cbd27d8b64
Joingl <joel.inglin@uzh.ch>
2023-09-04 13:13:11 +0200

    Minor bugfix.

--- src/main/java/com/example/automator/helper/CLIRunner.java
+++ src/main/java/com/example/automator/helper/CLIRunner.java
@@ -9,12 +9,13 @@ import org.apache.commons.lang3.SystemUtils;
 public class CLIRunner {
     public void runCommand(String cmd) {
         try {
-            System.out.println("Building process for Windows...");
             ProcessBuilder pb = new ProcessBuilder();
             //System.out.println(System.getenv()); //Prints SYSTEMPATH, enables manual check if NetLogo is part of it.
             if (SystemUtils.IS_OS_WINDOWS) {
+                System.out.println("Building process for Windows...");
                 pb.command("cmd.exe", "/c", "behaviorsearch_headless.bat " + cmd);
-            } else if (SystemUtils.IS_OS_LINUX) {
+            } else if (SystemUtils.IS_OS_UNIX) {
+                System.out.println("Building process for UNIX-System...");
                 pb.command("/bin/bash", "-c", "behaviorsearch_headless.sh " + cmd);
             } else {
                 throw new IOException("OS not compatible.");

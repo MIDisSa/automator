fc8ad4c3b6244c2c048787e0760b6235a590d214
hanuta27 <hannah.rohe@hispeed.ch>
2023-09-25 17:48:26 +0200

    test runABM with mocked netlogo workspace

--- src/test/java/com/example/automator/ABMRunnerTest.java
+++ src/test/java/com/example/automator/ABMRunnerTest.java
@@ -1,11 +1,15 @@
 package com.example.automator;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.*;
 
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.nlogo.headless.HeadlessWorkspace;
 
 import com.example.automator.helper.ModelInput;
 import com.example.automator.helper.Parameters;
@@ -50,6 +54,33 @@ public class ABMRunnerTest {
         // assert
         assertNotNull(result);
         assertEquals(4, result.size());
-        
+    }
+
+    @Test
+    public void testRunABMWithMockedWorkspace() {
+        // mock worksapce
+        HeadlessWorkspace workspace = mock(HeadlessWorkspace.class);
+        when(workspace.report(anyString())).thenReturn(64.0);
+        when(workspace.report("count turtles with [adoption_state = 1]")).thenReturn(1.0);
+        when(workspace.report("count turtles with [adoption_state = 2]")).thenReturn(2.0);
+
+        // mock result
+        ArrayList<Double> awareFarmersPerTick = new ArrayList<Double>();
+        ArrayList<Double> adoptersPerTick = new ArrayList<Double>();
+        for (int i = 0; i < mockModelInput.getNumberOfTicks(); i++) {
+            awareFarmersPerTick.add(1.0);
+            adoptersPerTick.add(2.0);
+        }
+
+        // call runABM with mocked workspace
+        ArrayList<String> result = ABMRunner.runABM(mockParameters, mockModelInput, workspace);
+
+        // assert
+        assertNotNull(result);
+        assertEquals(4, result.size());
+        assertEquals("1.0", result.get(0));
+        assertEquals("2.0", result.get(1));
+        assertEquals(awareFarmersPerTick.toString(), result.get(2));
+        assertEquals(adoptersPerTick.toString(), result.get(3));
     }
 }

6f2ddbaa3fb1e7eb4d9c15ddcd396ed25755f4ab
hanuta27 <hannah.rohe@hispeed.ch>
2023-09-26 09:26:25 +0200

    add endpoint test

--- src/test/java/com/example/automator/AutomatorControllerTests.java
+++ src/test/java/com/example/automator/AutomatorControllerTests.java
@@ -1,33 +1,45 @@
 package com.example.automator;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
-import java.util.ArrayList;
+import javax.print.attribute.standard.Media;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.http.MediaType;
 
-import com.example.automator.helper.ABMRunner;
-import com.example.automator.helper.CSVReader;
-
-
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AutomatorControllerTests {
-    @InjectMocks
-    private AutomatorController automatorController;
 
-    @Mock
-    private CSVReader csvReader;
-
-    @Mock
-    private ABMRunner abmRunner;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    public void testModelResults() {
+    public void testIndex() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+        .get("/")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("Greetings from Spring Boot!"));
+    }
 
+    @Test
+    public void testModelResults() throws Exception {
+        // Create a sample ModelInput object as JSON
+        String inputJson = "{\"numberOfTicks\": \"360\", \"frequencyDirectAd\": \"50\", \"kindOfIntervention\": \"direct_village_intervention\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders
+            .post("/results")
+            .content(inputJson)
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.awareFarmers").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.adopters").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.awareFarmersPerTick").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.adoptersPerTick").exists());
     }
 }

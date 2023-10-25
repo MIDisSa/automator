b2c77b7629c1af4121ee972b2d48281f91fc3c3e
hanuta27 <hannah.rohe@hispeed.ch>
2023-10-25 17:24:03 +0200

    open netlogo with application

--- src/main/java/com/example/automator/AutomatorApplication.java
+++ src/main/java/com/example/automator/AutomatorApplication.java
@@ -3,10 +3,13 @@ package com.example.automator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.nlogo.app.App;
+
 @SpringBootApplication
 public class AutomatorApplication {
 
 	public static void main(String[] args) {
+		App.main(args);
 		SpringApplication.run(AutomatorApplication.class, args);
 	}
 }

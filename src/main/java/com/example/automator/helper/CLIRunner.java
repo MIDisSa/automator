4c208e48acebc6ec0c025bb5bb4660915fef8e04
hanuta27 <hannah.rohe@hispeed.ch>
2023-12-19 09:58:20 +0100

    function to return lines from optimizer process

--- src/main/java/com/example/automator/helper/CLIRunner.java
+++ src/main/java/com/example/automator/helper/CLIRunner.java
@@ -7,6 +7,13 @@ import java.lang.ProcessBuilder;
 import org.apache.commons.lang3.SystemUtils;
 
 public class CLIRunner {
+
+    private StringBuilder outputBuffer;
+
+    public CLIRunner() {
+        outputBuffer = new StringBuilder();
+    }
+
     public void runCommand(String cmd) {
         try {
             ProcessBuilder pb = new ProcessBuilder();
@@ -15,7 +22,7 @@ public class CLIRunner {
                 pb.command("cmd.exe", "/c", "behaviorsearch_headless.bat " + cmd);
             } else if (SystemUtils.IS_OS_UNIX) {
                 System.out.println("Building process for UNIX-System...");
-                pb.command("/bin/bash", "-c", "./NetLogo-6.2.2/app/behaviorsearch/behaviorsearch_headless.sh " + cmd);
+                pb.command("/bin/bash", "-c", "/home/hannah/applications/netlogo/app/behaviorsearch/behaviorsearch_headless.sh " + cmd);
 
             } else {
                 throw new IOException("OS not compatible.");
@@ -26,11 +33,16 @@ public class CLIRunner {
                 
             String line;
             while ((line = reader.readLine()) != null) {
+                outputBuffer.append(line + "\n");
                 System.out.println(line);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public String getOutputBuffer() {
+        return outputBuffer.toString();
     }
 }

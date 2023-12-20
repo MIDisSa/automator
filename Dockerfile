3f1fa7a344dcdb6235746779c028da6f9961abe1
Joingl <joel.inglin@uzh.ch>
2023-12-20 13:53:20 +0100

    add abortOptimization endpoint

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt-get update && apt-get install -y \
     wget \
     unzip \
     bash \
+    procps \
     python3 \
     python3-dev \
     python3-pip
--- src/main/java/com/example/automator/AutomatorController.java
+++ src/main/java/com/example/automator/AutomatorController.java
@@ -19,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -372,6 +373,19 @@ public class AutomatorController {
         Parameters parameters = new CSVReader().parseDataCSV(CSV_FILE_PATH_DATA);
 
         return parameters;
+    }
+
+    @PutMapping("/abortOptimization")
+    @ResponseStatus(HttpStatus.OK)
+    public void abortOptimizationv2() {
+        try {
+            ProcessBuilder pb = new ProcessBuilder();
+            System.out.println("Killing process \"java -D...\"");
+            pb.command("/bin/bash", "-c", "pkill -f 'java -D'");
+            pb.start();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 
